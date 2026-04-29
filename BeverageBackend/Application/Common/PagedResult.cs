420fded4c61931b9af9fe196b5eb2e5ed683946f
thanhthuan173 <thanhthuan009753@gmail.com>
2026-04-29 20:02:01 +0700

    feat(common): add base classes for pagination and query parameters

---
+++ BeverageBackend/Application/Common/PagedResult.cs
@@ -0,0 +1,19 @@
+﻿namespace BeverageBackend.Application.Common
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+
+        public PagedResult(IEnumerable<T> items, int totalCount, int pageNumber, int pageSize)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
---
+++ BeverageBackend/Application/Common/Query/QueryParameters.cs
@@ -0,0 +1,32 @@
+﻿namespace BeverageBackend.Application.Common.Query
+{
+    public class QueryParameters
+    {
+        private const int MaxPageSize = 30;
+        private int pageSize = 10;
+        private int pageNumber = 1;
+
+        public int PageNumber 
+        {
+            get => pageNumber;
+            set => pageNumber = value < 1 ? 1 : value;
+        }
+
+        public int PageSize 
+        {
+            get => pageSize;
+            set
+            {
+                if (pageSize < 1)
+                    pageSize = 10;
+                else if (pageSize > MaxPageSize)
+                    pageSize = MaxPageSize;
+                else
+                    pageSize = value;
+            }
+        }
+
+        public string? SortBy { get; set; }
+        public bool Desc { get; set; } = false;
+    }
+}
