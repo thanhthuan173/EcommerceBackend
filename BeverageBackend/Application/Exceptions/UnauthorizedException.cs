6fb6ba7369d0df9e16276358ca679703aecf2c96
thanhthuan173 <thanhthuan009753@gmail.com>
2026-05-28 22:59:29 +0700

    feat: add UnauthorizedException

---
+++ BeverageBackend/Application/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,9 @@
+﻿namespace BeverageBackend.Application.Exceptions
+{
+    public class UnauthorizedException : AppException
+    {
+        public UnauthorizedException(string message) : base(message, 401, "Unauthorized")
+        {
+        }
+    }
+}
