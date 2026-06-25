2cf179dde29577cf57d9b6b5389407d4e59f2371
Thuan Pham <thanhthuan009753@gmail.com>
2026-06-25 14:09:02 +0700

    feat(user): add FluentValidation for update and create product dto

---
+++ BeverageBackend/Application/Validators/Product/CreateProductDtoValidator.cs
@@ -0,0 +1,33 @@
+using BeverageBackend.Application.Dto.Product;
+using FluentValidation;
+
+namespace BeverageBackend.Application.Validators.Product
+{
+    public class CreateProductDtoValidator : AbstractValidator<CreateProductDto>
+    {
+        public CreateProductDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must not over 100 characters.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(500).WithMessage("Description must not over 500 characters.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Invalid price.");
+
+            RuleFor(x => x.Stock)
+                .GreaterThanOrEqualTo(0).WithMessage("Invalid stock quantity.");
+
+            RuleFor(x => x.ImgUrl)
+                .NotEmpty().WithMessage("Image URL is required.")
+                .Must(url => Uri.TryCreate(url, UriKind.Absolute, out _))
+                .WithMessage("Invalid image URL.");
+
+            RuleFor(x => x.CategoryId)
+                .GreaterThan(0).WithMessage("Invalid category.");
+        }
+    }
+}
