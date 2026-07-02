672f397ae0cb6b5e051036ff9a48ccf13686f181
Thuan Pham <thanhthuan009753@gmail.com>
2026-07-02 20:21:03 +0700

    chore: ignore log files and remove unused usings

--- BeverageBackend/Program.cs
+++ BeverageBackend/Program.cs
@@ -1,25 +1,7 @@
-using BeverageBackend.API.Configurations;
 using BeverageBackend.API.Extensions;
-using BeverageBackend.API.Middlewares;
-using BeverageBackend.API.Services;
 using BeverageBackend.Application;
-using BeverageBackend.Application.Interfaces;
-using BeverageBackend.Application.Interfaces.Services;
-using BeverageBackend.Application.Services;
-using BeverageBackend.Application.Services.Auth;
-using BeverageBackend.Application.Validators.Auth;
 using BeverageBackend.Infrastructure;
-using BeverageBackend.Infrastructure.Persistence;
-using BeverageBackend.Infrastructure.Repository;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using Serilog;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
